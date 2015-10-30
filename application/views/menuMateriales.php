4dd77e8aa71687aeccff1ed5cf7dba839a453f94
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-10-30 17:26:05 -0600

    Menus Responsivos
    Se hicieron los menus para que se estuvieran separados el menu de
    opciones de materiales, y el menu principal

--- application/views/menuMateriales.php
+++ application/views/menuMateriales.php
@@ -1,8 +1,14 @@
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header col-lg-2">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#opcionesMateriales"> <!-- Menu desplegable para opciones de materiaales -->
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#opcionesMenu"> <!-- Menu desplegable para opciones de menu -->
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -11,21 +17,20 @@
         <a class="navbar-brand textTitle" href="index.php/welcome/panelPrincipal"><i class="fa fa-home"></i></a>
         <a class="navbar-brand textTitle" href="index.php/welcome/panelMateriales">Materials</a>
     </div>
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
-    <ul class="nav navbar-nav textnav ">
-        <li><a href="">Archivo</a></li>
-        <li><a href="charts.html">Registros</a></li>
-        <li><a href="tables.html">Control</a></li>
-        <li><a href="forms.html">Reporte</a></li>
-        <li><a href="bootstrap-elements.html">Seguridad</a></li>
-        <li><a href="blank-page.html">Productos</a></li>
-        <li><a href="index-rtl.html">Pedidos</a></li>
-        <li><a href="bootstrap-elements.html">Programación</a></li>
-        <li><a href="bootstrap-grid.html">Clientes</a></li>
-        <li><a href="blank-page.html">Ventas</a></li>
-        <li><a href="index-rtl.html">Categorias</a></li>
-    </ul>
-    <div>
+    <div class="collapse navbar-collapse navbar-ex1-collapse" id="opcionesMenu">
+        <ul class="nav navbar-nav textnav ">
+            <li><a href="">Archivo</a></li>
+            <li><a href="charts.html">Registros</a></li>
+            <li><a href="tables.html">Control</a></li>
+            <li><a href="forms.html">Reporte</a></li>
+            <li><a href="bootstrap-elements.html">Seguridad</a></li>
+            <li><a href="blank-page.html">Productos</a></li>
+            <li><a href="index-rtl.html">Pedidos</a></li>
+            <li><a href="bootstrap-elements.html">Programación</a></li>
+            <li><a href="bootstrap-grid.html">Clientes</a></li>
+            <li><a href="blank-page.html">Ventas</a></li>
+            <li><a href="index-rtl.html">Categorias</a></li>
+        </ul>
             <!-- Top Menu Items -->
         <ul class="nav navbar-right top-nav ">
             <li class="dropdown ">
@@ -44,10 +49,10 @@
                 </ul>
             </li>
         </ul>
-    <!-- </div> -->
+    </div>
 
     <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <div class="collapse navbar-collapse navbar-ex1-collapse" id="opcionesMateriales">
         <ul class="nav navbar-nav side-nav textnav">
             <li>
                 <a href="index.php/welcome/matProductos"> Productos</a>
